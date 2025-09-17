8e9d3f8e1ad05949964926ce1defee4902c4b257
Star <mmdakane@rocketmail.com>
2025-09-17 17:57:19 +0200

    restarting my script by compartmentalising

--- script.js
+++ script.js
@@ -20,22 +20,11 @@ function getTimeframe(e) {
 const populateDom = (data) => {
     data.forEach((item) => {
         appendItem(item);
-        // const img = listItem.querySelector(".img");
-        // let arr = item.title.toLowerCase().split(" ");
-        // let str = arr.join("-");
-        // img.style.backgroundImage = "./images/icon-${item.title.toLowerCase().${str}.svg}"
-
-
     });
 }
 
 const appendItem = (item) => {
     const listItem = document.createElement("li");
-    let timeframe = "";
-
-    timeframe = buttonCon.querySelectorAll("button").forEach((button) => {
-        button = getTimeframe;
-    })
 
     listItem.innerHTML = `
     <li class="item card">
@@ -55,20 +44,7 @@ const appendItem = (item) => {
           </div>
         </li> 
     `
-    container.appendChild(listItem)
-    // const current = listItem.querySelector(".current");
-    // const previous = listItem.querySelector(".previous");
-
-    // if (timeframe === "daily") {
-    //     current.textContent = `${item.timeframes.timeframe.current}`;
-    //     previous.textContent = `Last ${item.timeframes.timeframe.previous}`;
-    // } else if (timeframe === "weekly") {
-    //     current.textContent = `${item.timeframes.timeframe.current}`;
-    //     previous.textContent = `Last Week ${item.timeframes.timeframe.previous}`;
-    // } else if (timeframe === "monthly") {
-    //     current.textContent = `${item.timeframes.timeframe.current}`;
-    //     previous.textContent = `Last Month ${item.timeframes.timeframe.previous}`;
-    // }
+    container.appendChild(listItem);
 
 }
 
